7a53947c2093a5b7eec3327dc0f3b915676c4413
Arthur Damann <arthurdamanncompany@gmail.com>
2025-08-21 11:53:46 -0300

    Python

---
+++ Python/functions.py
@@ -0,0 +1,16 @@
+x = "Gremio"
+
+def funcao():
+    global y
+    y = "Flamengo"
+    print(x)
+
+funcao()
+
+print(y)
+
+x = str(1)
+print(type(x))
+
+y = int(x)
+print(y)
---
+++ Python/testes.py
@@ -0,0 +1,31 @@
+x = 5
+y = 2
+
+# comentario
+
+"""
+aesgwegweg"""
+j = "jhon"
+k = "kelly"
+if x > y:
+    print("x is greater than y")
+
+
+
+print(j)
+print(k)
+
+print(type(x))
+
+myvar2 = "John"
+
+m1, m2, m3 =  "1", "2", "3"
+
+print(m1, m2, m3)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
