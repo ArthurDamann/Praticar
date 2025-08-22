905a2e1dbe84bd91724131e3d4d3c161064617e8
Arthur Damann <arthurdamanncompany@gmail.com>
2025-08-22 12:17:53 -0300

    Advinhação

---
+++ Python/Advinhação.py
@@ -0,0 +1,21 @@
+import random
+
+def adivinhacao():
+    print("Bem-vindo ao jogo de adivinhação!")
+    numero_secreto = random.randint(1, 100)
+    tentativas = 0
+    acertou = False
+
+    while not acertou:
+        palpite = int(input("Digite um número entre 1 e 100: "))
+        tentativas += 1
+
+        if palpite < numero_secreto:
+            print("Muito baixo! Tente novamente.")
+        elif palpite > numero_secreto:
+            print("Muito alto! Tente novamente.")
+        else:
+            acertou = True
+            print(f"Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas.")
+
+adivinhacao()
